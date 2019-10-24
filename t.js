37ae9b4135785e63f339c42d51485d4aa6ea2c9d
ofir h <houbara0@gmail.com>
2019-10-24 11:29:08 +0300

    remove t.js

--- t.js
+++
@@ -1,6 +0,0 @@
-const str =
-    'C161_996557606 admin_2019-06-12 10-53-50_BR101723 Quantitation Ct Results.csv';
-
-const m = str.split('_');
-const [a, b, c, d] = m;
-console.log(d.replace(/[ -].*/g, ''));
