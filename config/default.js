04d5e5ac447f54212ed7412be099a514b7a5a9c0
ofir h <houbara0@gmail.com>
2019-10-24 11:30:26 +0300

    remove all

--- config/default.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    tempDir: path.resolve(__dirname, '..', 'TEMPֹ_FILE'),
-    seegeneFilesDir: path.resolve(__dirname, '..', 'ARCHIVE_SEEGENE_FILES'),
-};
